d11594052e9813043a9a221d51a081d3cc28f1a0
frankevinicius <vinicius8376@gmail.com>
2019-05-23 14:26:07 -0300

    enviar repositorio

--- servidor.py
+++ servidor.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, session, redirect
 app=Flask(__name__)
-app.config["SECRET_KEY"]= "jogo"
+#app.config["SECRET_KEY"]= "jogo"
+
 class jogo:
     def __init__(self,nome,genero,ano_de_criacao):
         self.nome = nome
@@ -9,5 +10,15 @@ class jogo:
 
 @app.route("/")
 def inicio():
-    return
-render_template("inicio.html")
+    return render_template("inicio.html")
+
+@app.route("/listar_jogos")
+def listar_jogos():
+    return render_template("listar_jogos.html")
+
+@app.route("/incluir_jogos")
+def incluir_jogos():
+    return render_template("incluir_jogos.html")
+    
+
+app.run(debug=True, host="0.0.0.0")
